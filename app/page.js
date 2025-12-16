b0798e4b9319ebbdea41af7d419c3442b35cb591
Sebcoach13 <140277344+Sebcoach13@users.noreply.github.com>
2025-12-16 02:18:33 +0100

    fix: Renommage de la fonction de page en Page() pour le App Router

--- app/page.js
+++ app/page.js
@@ -1,31 +1,18 @@
 // app/page.js
 
-// Importation des composants
-import Header from '../components/Header'; 
-import Hero from '../components/Hero';     
-import Skills from '../components/Skills'; 
-import Projects from '../components/Projects'; 
-import Contact from '../components/Contact'; 
-export default function Home() {
+export default function Page() {
   return (
     <div className="min-h-screen bg-white">
-      
       {/* 1. La Barre de Navigation */}
       <Header />
-      
+
       {/* 2. Le Contenu Principal */}
       <main>
         <Hero />
         <Skills />
         <Projects />
-        <Contact /> 
-
+        <Contact />
       </main>
-      
-      {/* 3. Le Pied de Page */}
-      <footer className="bg-gray-800 text-center text-white p-4">
-          &copy; {new Date().getFullYear()} DevCodeMaster_13. Tous droits réservés.
-      </footer>
     </div>
-  );
+  )
 }
